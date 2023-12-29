0d45a74054d3ce42596f085585808b7a01a02797
Eloy85 <eloyfmolina@gmail.com>
2023-12-29 18:31:43 -0300

    correcciones

--- Abcblog/settings.py
+++ Abcblog/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default=config('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG',default=True,cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
